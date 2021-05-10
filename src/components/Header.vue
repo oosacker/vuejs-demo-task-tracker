9ca9320dbdb670047085b47fedf37fe0ff26e380
Natsuki Hasegawa <oosacker@gmail.com>
2021-05-10 19:29:42 +1200

    show button only on home

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,7 @@
     <header>
         <h1>{{ title }}</h1>
         <Button 
+            v-show="homePage" 
             :text="showAddTask ? 'Hide' : 'Add Task'" 
             :color="showAddTask ? 'Red' : 'Green'" 
             @btn-click="$emit('toggle-add-task')"
@@ -26,6 +27,16 @@
         },
         methods: {
             
+        },
+        computed : {
+            // use v-show to decide to show button or not depending on path
+            homePage() {
+                if(this.$route.path === '/') {
+                    return true
+                } else {
+                    return false
+                }
+            }
         }
     }
 </script>
